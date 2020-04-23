bcbbccaa639766e78a8c53447f80facd380e37bb
Kwan <kks0211@hanmail.net>
2020-04-23 23:35:25 +0900

    20200424 안드로이드 뷰

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        setContentView(R.layout.scrollerview_13)
+        setContentView(R.layout.practice_15)
     }
 }
