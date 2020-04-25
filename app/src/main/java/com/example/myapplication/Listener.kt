d334d7304d978c54da0f5876e63ecc7ea5ed2b96
Kwan <kks0211@hanmail.net>
2020-04-25 20:18:07 +0900

    20200424 안드로이드 Activity

---
+++ app/src/main/java/com/example/myapplication/Listener.kt
@@ -0,0 +1,51 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_listener.*
+
+class Listener : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listener)
+
+        //뷰를 activity로 가져오는 방법
+        //1. 직접찾음
+        //val textView : TextView = findViewById(R.id.hello)
+
+        //2. xml을 import해서 가져옴 (람다식)
+        // 표현1
+        hello.setOnClickListener {
+            Log.d("click", "Click!!")
+        }
+
+        //표현 2 (익명함수 방식)
+        hello.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Log.d("test", "testClick!1")
+            }
+        })
+        //표현 3
+        val click = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                var num = 10
+                //Log.d("test", "testClick!2")
+                //뷰를 조작하는 함수
+                hello.setText("안녕하세요.")
+                image.setImageResource(R.drawable.man)
+                num += 10
+                Log.d("test", "testClick!2")
+                Log.d("num",""+num)
+
+            }
+        }
+        hello.setOnClickListener(click)
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +10,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         setContentView(R.layout.practice_15)
+        Log.d("life_cylcle", "onCreate")
+    }
+
+    override fun onStart() {
+        Log.d("life_cylcle", "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("life_cylcle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("life_cylcle", "onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("life_cylcle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("life_cylcle", "onDestroy")
     }
 }
